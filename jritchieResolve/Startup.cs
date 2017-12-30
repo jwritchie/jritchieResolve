52c04c8d65705b9bb871bef5f4d8fb537552d6b5
jwritchie <JRitchie.NC@gmail.com>
2017-12-30 16:00:21 -0500

    Initial commit

---
+++ jritchieResolve/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jritchieResolve.Startup))]
+namespace jritchieResolve
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
